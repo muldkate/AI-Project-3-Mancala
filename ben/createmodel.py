9c62833a5b705692af04b3880f738a8d17c7b724
dev <dev@debian>
2017-04-07 12:47:24 -0400

    rudimentary ML bot

--- ben/createmodel.py
+++ ben/createmodel.py
@@ -1,11 +1,18 @@
 import numpy as np
 from sklearn import svm
+from sklearn.externals import joblib
 
 a = open('games_data.txt', 'r')
 b = open('games_targets.txt', 'r')
 data = np.loadtxt(a)
 targets = np.loadtxt(b)
 clf = svm.SVC()
+print("Creating a model...")
+clf.fit(data, targets)
+print("Model complete.")
+print("Saving model to model.pkl")
+joblib.dump(clf, 'model.pkl')
+print("Model saved")
 #TODO: Play with SVC() Parameters
 #TODO: Store model
 #TODO: Cross validation
