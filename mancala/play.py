6d6bb36f7e9c305746e1edf9ec3e5b585c18cb5c
MattiePhillips <mphillips1695@gmail.com>
2017-04-08 22:19:05 -0400

    Initial GA commit

--- mancala/play.py
+++ mancala/play.py
@@ -7,18 +7,27 @@ try:
     from .ai_profiles import VectorAI
     from .ai_profiles import RandomAI
     from ai_profiles import LeftmostAI
+#    from .ga_bot import GeneticAlgorithmAI
 except Exception: #ImportError
     from mancala import Match
     from mancala import HumanPlayer
     from ai_profiles import VectorAI
     from ai_profiles import RandomAI
     from ai_profiles import LeftmostAI
+#    from ga_bot import GeneticAlgorithmAI
 
 
-def main():
+def main(player1=None, player2=None):
     """ Script to begin a match of Mancala. """
     print ("Welcome to Mancala!")
-    match = Match(player1_type=HumanPlayer, player2_type=LeftmostAI, param_print_game_status=True)
+
+    # (optional) dynamic player types
+    if player1 is None:
+        player1 = RandomAI
+    if player2 is None:
+        player2 = LeftmostAI
+
+    match = Match(player1_type=player1, player2_type=player2, param_print_game_status=True)
     match.handle_next_move()
    
 
